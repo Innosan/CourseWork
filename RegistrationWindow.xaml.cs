31a48fac94cdf57c92d88405c19094f5fa616cb9
InnoSan <reau_beua@gmail.com>
2022-03-28 01:05:22 +0300

    added sign up window better placeholder text

---
+++ RegistrationWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace CourseWork
+{
+    /// <summary>
+    /// Логика взаимодействия для RegistrationWindow.xaml
+    /// </summary>
+    public partial class RegistrationWindow : Window
+    {
+        public RegistrationWindow()
+        {
+            InitializeComponent();
+
+            passBox.GotFocus += new RoutedEventHandler(passBox_Focus);
+
+            passBox.Password = "samplepass";
+        }
+
+        private void passBox_Focus(object sender, RoutedEventArgs e)
+        {
+            passBox.Password = "";
+
+            passBox.Foreground = Brushes.White;
+        }
+
+        private void toSignInHypLink_Click(object sender, RoutedEventArgs e)
+        {
+            var newForm = new MainWindow();
+
+            newForm.Show();
+            this.Close();
+        }
+    }
+}
