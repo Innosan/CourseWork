de5dba18e76c68948e2ec6c151cbcdd486bc3bb8
InnoSan <reau_beua@gmail.com>
2022-06-15 18:58:00 +0300

    some reorganisation and cringie refactoring

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
@@ -17,13 +16,10 @@ namespace CourseWork
     {
         ApplicationContext db;
 
-        public ObservableCollection<Product> itemsList { get; set; } = new ObservableCollection<Product>();
-        public ICollectionView ItemsView { get { return CollectionViewSource.GetDefaultView(itemsList); } }
+        public ObservableCollection<Product> ItemsList { get; set; } = new ObservableCollection<Product>();
+        public ICollectionView ItemsView { get { return CollectionViewSource.GetDefaultView(ItemsList); } }
 
-        public List<Categorie> categoriesList { get; set; } = new List<Categorie>();
-
-        public string[] publicUserName;
-        public int publicUserRole;
+        public List<Categorie> CategoriesList { get; set; } = new List<Categorie>();
 
         private string search;
         public string Search
@@ -31,7 +27,6 @@ namespace CourseWork
             get { return search; }
             set { search = value; NotifyPropertyChanged("Search"); ItemsView.Refresh(); }
         }
-
 
         public event PropertyChangedEventHandler PropertyChanged;
         private void NotifyPropertyChanged(string propertyName) { 
@@ -48,19 +43,6 @@ namespace CourseWork
 
             ItemsView.Filter = new Predicate<object>(o => Filter(o as Product));
 
-            publicUserName = userName.Split(' ');
-            publicUserRole = userRole;
-
-            if (userRole == 1)
-            {
-                MessageBox.Show("Вы вошли как админ!");
-            }
-            else
-            {
-                MessageBox.Show("Ты бедный пользователь, ухади!(");
-                addProductBtn.Visibility = Visibility.Collapsed;
-            }
-
             UpdateItemsList();
 
             using (ApplicationContext context = new ApplicationContext())
@@ -69,46 +51,10 @@ namespace CourseWork
 
                 foreach (Categorie categorie in categories)
                 {
-                    categoriesList.Add(categorie);
+                    CategoriesList.Add(categorie);
                 }
             }
-        }
-
-        private bool Filter(Product product)
-        {
-            return Search == null
-                || product.ProdName.IndexOf(Search, StringComparison.OrdinalIgnoreCase) != -1
-                || product.ProdManufacturer.IndexOf(Search, StringComparison.OrdinalIgnoreCase) != -1
-                || product.ProdDescription.IndexOf(Search, StringComparison.OrdinalIgnoreCase) != -1;
-        }
-
-        private void FilterView(ToggleButton sender)
-        {
-            if (sender.IsChecked == true)
-            {
-                ItemsView.Filter = product => ((Product)product).ProdCategorie == sender.Content.ToString();
-            }
-            else
-            {
-                ItemsView.Filter = null;
-            }
-            
-        }
-
-        private void UpdateItemsList()
-        {
-            itemsList.Clear();
-
-            using (ApplicationContext context = new ApplicationContext())
-            {
-                var products = context.Products;
-
-                foreach (Product product in products)
-                {
-                    itemsList.Add(product);
-                }
-            }
-        }
+        }       
 
         private void goBackBtn_Click(object sender, RoutedEventArgs e)
         {
@@ -145,45 +91,29 @@ namespace CourseWork
 
             if (nameResults.IsValid == false)
             {
-                var tt = new ToolTip();
-
-                tt.Content = nameResults.Errors[0].ErrorMessage;
-
-                nameTextBox.Background = (Brush)(new BrushConverter().ConvertFrom("#59E1452C"));
-                nameTextBox.ToolTip = tt;
+                SetInvalidUI(nameTextBox, nameResults);
             }
             else
             {
-                nameTextBox.Background = Brushes.Transparent;
-                nameTextBox.ToolTip = null;
+                SetValidUI(nameTextBox);
             }
 
             if(descResults.IsValid == false)
             {
-                var tt = new ToolTip();
-                tt.Content= descResults.Errors[0].ErrorMessage;
-
-                descTextBox.Background = (Brush)(new BrushConverter().ConvertFrom("#59E1452C"));
-                descTextBox.ToolTip = tt;
+                SetInvalidUI(descTextBox, descResults);
             }
             else
             {
-                descTextBox.Background = Brushes.Transparent;
-                descTextBox.ToolTip = null;
+                SetValidUI(descTextBox);
             }
 
             if (manufactResults.IsValid == false)
             {
-                var tt = new ToolTip();
-                tt.Content = manufactResults.Errors[0].ErrorMessage;
-
-                manufactTextBox.Background = (Brush)(new BrushConverter().ConvertFrom("#59E1452C"));
-                manufactTextBox.ToolTip = tt;
+                SetInvalidUI(manufactTextBox, manufactResults);
             }
             else
             {
-                manufactTextBox.Background = Brushes.Transparent;
-                manufactTextBox.ToolTip = null;
+                SetValidUI(manufactTextBox);
             }
 
             if (nameResults.IsValid && descResults.IsValid && manufactResults.IsValid)
@@ -193,10 +123,69 @@ namespace CourseWork
 
                 ClosePopUp();
 
-                MessageBox.Show("Продукт добавлен успешно!");
-
                 UpdateItemsList();
             }
+        }
+
+        private void closePopUp_Click(object sender, RoutedEventArgs e)
+        {
+            ClosePopUp();
+        }
+
+        private void FilterTab_Click(object sender, RoutedEventArgs e)
+        {
+            FilterView((ToggleButton)sender);
+        }
+
+        private void UpdateItemsList()
+        {
+            ItemsList.Clear();
+
+            using (ApplicationContext context = new ApplicationContext())
+            {
+                var products = context.Products;
+
+                foreach (Product product in products)
+                {
+                    ItemsList.Add(product);
+                }
+            }
+        }
+        private bool Filter(Product product)
+        {
+            return Search == null
+                || product.ProdName.IndexOf(Search, StringComparison.OrdinalIgnoreCase) != -1
+                || product.ProdManufacturer.IndexOf(Search, StringComparison.OrdinalIgnoreCase) != -1
+                || product.ProdDescription.IndexOf(Search, StringComparison.OrdinalIgnoreCase) != -1;
+        }
+
+        private void FilterView(ToggleButton sender)
+        {
+            if (sender.IsChecked == true)
+            {
+                ItemsView.Filter = product => ((Product)product).ProdCategorie == sender.Content.ToString();
+            }
+            else
+            {
+                ItemsView.Filter = null;
+            }
+        }
+
+        private void SetInvalidUI(TextBox textBox, FluentValidation.Results.ValidationResult result)
+        {
+            ToolTip toolTip = new ToolTip
+            {
+                Content = result.Errors[0].ErrorMessage
+            };
+
+            textBox.Background = (Brush)(new BrushConverter().ConvertFrom("#59E1452C"));
+            textBox.ToolTip = toolTip;
+        }
+
+        private void SetValidUI(TextBox textBox)
+        {
+            textBox.Background = Brushes.Transparent;
+            textBox.ToolTip = null;
         }
 
         private void ClosePopUp()
@@ -217,16 +206,6 @@ namespace CourseWork
             quantityTextBox.Text = "";
 
             addProdPopup.IsOpen = false;
-        }
-
-        private void closePopUp_Click(object sender, RoutedEventArgs e)
-        {
-            ClosePopUp();
-        }
-
-        private void FilterTab_Click(object sender, RoutedEventArgs e)
-        {
-            FilterView((ToggleButton)sender);
         }
     }
 }
